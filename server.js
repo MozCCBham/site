331b69e1f6156a6ebacac39ae749739495dc75d3
BuildTools <jmtrik@gmail.com>
2017-01-22 18:14:42 +0000

    Added framework for mail body

--- server.js
+++ server.js
@@ -16,24 +16,28 @@ var transporter = nodemailer.createTransport(up);
 var mailOptions = {
             from: '"worker" < '+ configMailer.username + '>', // sender address
             to: 'jmtrik@gmail.com', // list of receivers
-            subject: 'Message for MozCCBham ✔', // Subject line
-            text: 'You have been sent an message.', // plaintext body
-            html: '<b>You have been sent an message.</b>' // html body
+            subject: 'Message for MozCCBham ✔' // Subject line
           };
 
-console.log(mailOptions);
+
 
 app.use(express.static('public'))
 
 app.post('/mail', function (req, res) {
   //res.send('Mail sent')
   // send mail with defined transport object
+    mailOptions.text = 'You have been sent an message.<br>'; // plaintext body
+    mailOptions.html = '<b>You have been sent an message.</b>'; // html body
+    
+    console.log(mailOptions);
           transporter.sendMail(mailOptions, function(error, info){
             if(error){
               return console.log(error);
             }
+            res.send('Mail sent');
             console.log('Message sent: ' + info.response);
           });
+
 
 })
 
